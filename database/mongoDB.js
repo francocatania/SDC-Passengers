c9181419301bd6cac4b989f0f3ffafdb6a0cc278
Franco Catania <catania.fco@gmail.com>
2018-02-02 16:15:25 -0800

    mongo waitingList

---
+++ database/mongoDB.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/sdc_passengers');
+
+var mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, 'connection error:'));
+mongoDB.once('open', function() {
+  console.log('we\'re connected!');
+});
+
+var waitingUserSchema = mongoose.Schema({
+  userId: { type: Number, required: true, index: true, unique: true },
+  created_at: { type: Date, default: Date.now },
+  origin: Array,
+  destination: Array,
+});
+var waitingUser = mongoose.model('waiting user', waitingUserSchema);
+
+
+module.exports.mongoDB = mongoDB;
+module.exports.waitingUser = waitingUser;
